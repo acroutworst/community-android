0d5a34accf0d02a92cdaf4982ca794168bf2242a
acroutworst <croutworsta@gmail.com>
2017-01-06 17:45:28 -0800

    added login and splash screens

---
+++ app/src/main/java/com/croutworst/community/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.croutworst.community;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(intent);
+
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
