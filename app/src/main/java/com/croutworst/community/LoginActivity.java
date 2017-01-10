b3a42611917449052bd8437a4ceb18cd4f5109e9
acroutworst <croutworsta@gmail.com>
2017-01-09 23:00:08 -0800

    added home screen w/ tabbed views and updated login view

--- app/src/main/java/com/croutworst/community/LoginActivity.java
+++ app/src/main/java/com/croutworst/community/LoginActivity.java
@@ -3,6 +3,7 @@ package com.croutworst.community;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -189,6 +190,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
         }
     }
 
@@ -325,7 +329,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             }
 
-            // TODO: register the new account here.
+
+
             return true;
         }
 
