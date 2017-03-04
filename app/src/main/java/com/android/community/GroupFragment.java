fdf98142c67e9d38cb5efcd088d46d4f15bcd6e2
acroutworst <croutworsta@gmail.com>
2017-03-04 10:39:58 -0800

    cleaned up groupsfragment and added swipetorefresh for groups

--- app/src/main/java/com/android/community/GroupFragment.java
+++ app/src/main/java/com/android/community/GroupFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
@@ -34,52 +35,76 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class GroupFragment extends Fragment {
     final private String TAG = "GroupFragment";
 
+		/* Initialize Views */
     private RecyclerView recyclerView;
     private ViewGroup view;
     private GridAdapter adapter;
+		private SwipeRefreshLayout mSwipeRefreshLayout;
+		private FloatingActionButton fab;
 
-    @Nullable
+
+	@Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
         view = (ViewGroup) inflater.inflate(R.layout.fragment_group, container, false);
-        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
 
-        int spanCount = getSpanCount();
-
-        adapter = new GridAdapter();
-
-        // We are using a multi span grid to show many color models in each row. To set this up we need
-        // to set our span count on the adapter and then get the span size lookup object from
-        // the adapter. This look up object will delegate span size lookups to each model.
-        adapter.setSpanCount(spanCount);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), spanCount);
-        gridLayoutManager.setSpanSizeLookup(adapter.getSpanSizeLookup());
-        gridLayoutManager.setRecycleChildrenOnDetach(true);
-
-        recyclerView.setLayoutManager(gridLayoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.addItemDecoration(new VerticalGridCardSpacingDecoration());
-        recyclerView.setAdapter(adapter);
-
-        // Many color models are shown on screen at once. The default recycled view pool size is
-        // only 5, so we manually set the pool size to avoid constantly creating new views when
-        // the colors are randomized
-        recyclerView.getRecycledViewPool().setMaxRecycledViews(R.layout.model_group, 50);
-
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_group);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                adapter.onAddClicked();
-            }
-        });
-
+				initViews();
+				setupClickListeners();
 				queryGroupPost();
 
         return view;
     }
+
+		private void initViews() {
+			mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.group_swipe_refresh_layout);
+			mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+					android.R.color.holo_green_light,
+					android.R.color.holo_orange_light,
+					android.R.color.holo_red_light);
+			recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+			int spanCount = getSpanCount();
+
+			adapter = new GridAdapter();
+
+			// We are using a multi span grid to show many color models in each row. To set this up we need
+			// to set our span count on the adapter and then get the span size lookup object from
+			// the adapter. This look up object will delegate span size lookups to each model.
+			adapter.setSpanCount(spanCount);
+			GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), spanCount);
+			gridLayoutManager.setSpanSizeLookup(adapter.getSpanSizeLookup());
+			gridLayoutManager.setRecycleChildrenOnDetach(true);
+
+			recyclerView.setLayoutManager(gridLayoutManager);
+			recyclerView.setHasFixedSize(true);
+			recyclerView.addItemDecoration(new VerticalGridCardSpacingDecoration());
+			recyclerView.setAdapter(adapter);
+
+			// Many color models are shown on screen at once. The default recycled view pool size is
+			// only 5, so we manually set the pool size to avoid constantly creating new views when
+			// the colors are randomized
+			recyclerView.getRecycledViewPool().setMaxRecycledViews(R.layout.model_group, 50);
+		}
+
+		private void setupClickListeners() {
+			mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+				@Override
+				public void onRefresh() {
+					queryGroupPost();
+					mSwipeRefreshLayout.setRefreshing(false);
+				}
+			});
+
+			fab = (FloatingActionButton) view.findViewById(R.id.fab_group);
+			fab.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					queryGroupPost();
+				}
+			});
+		}
 
     private void queryGroupPost() {
         //Here a logging interceptor is created
